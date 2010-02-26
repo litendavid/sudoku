460816eff0a8cdf0a2f2472afdfb8cf9aa576d93
David Waller <david@auragon.net>
2010-02-26 09:03:39 +0100

    implementing functionality and tests (step4)

---
+++ development/testsuite.js
@@ -0,0 +1,155 @@
+/************** Test helper methods *********************************************************************/
+
+function objpropcounter(obj){
+	var i = 0;
+	for(var p in obj){
+		if (obj.hasOwnProperty(p)){
+			i++;			
+		}
+	}
+	return i;
+}
+
+/**************/ module("Static array methods"); /*******************************************************/
+
+test("Array extra methods are declared",function(){
+	equals(typeof Array.compare,"function","compare function exists");
+	equals(typeof Array.locate,"function","locate function exists");
+	equals(typeof Array.unique,"function","unique function exists");
+	equals(typeof Array.common,"function","common function exists");
+	equals(typeof Array.rince,"function","rince function exists");
+});
+
+test("compare function",function(){
+	ok(Array.compare([1,2,3],[1,2,3]),"should return true if arrays are identical");
+	ok(!Array.compare([1,2,3],[4,5,6]),"should return false if differnet members");
+	ok(!Array.compare([1,2,3],[1,2,3,3]),"should return false if 2nd is same but has more members");
+	ok(!Array.compare([1,2,3],[3,2,1]),"should return false if same but different order");
+});
+
+test("locate function",function(){
+	equals(Array.locate(6,[1,2,3,4,5]),-1,"should return -1 when needle doesn't exist");
+	equals(Array.locate(4,[1,2,3,4,5]),3,"should return index when needle exists");
+});
+
+test("unique function",function(){
+	ok(Array.compare(Array.unique([1,2,3,3,3,4,4,5]),[1,2,3,4,5]),"should return only unique members");
+});
+
+test("common function",function(){
+	ok(Array.compare(Array.common([1,2,3],[2,3,4,5]),[2,3]),"should only return common members");
+	ok(Array.compare(Array.common([1,2,3],[4,5,6]),[]),"should return empty array if no common members");
+});
+
+test("rince function",function(){
+	ok(Array.compare(Array.rince([1,2,3,4,5],[2,3,4]),[1,5]),"should remove 2nd array members from first");
+});
+
+/**************/ module("Singleton"); /*******************************************************/
+
+test("is exposed",function(){
+	equals(typeof window.SS,"object","as SS");
+});
+
+test("contains classes",function(){
+	equals(typeof SS.CandList,"function","CandList is declared");
+	equals(typeof SS.Square,"function","Square is declared");
+	equals(typeof SS.House,"function","House is declared");
+});
+
+/**************/ module("CandList"); /*******************************************************/
+
+test("instantiation",function(){
+	var c = new SS.CandList();
+	ok(Array.compare(c.cands,[666,1,1,1,1,1,1,1,1,1]),"defaults to containing all candidates");
+	equals(c.nbrCands,9,"has correct total");
+	c = new SS.CandList([666,0,0,0,0,0,0,0,1,1]);
+	ok(Array.compare(c.cands,[666,0,0,0,0,0,0,0,1,1]),"can be instantiated with given candidates");
+	equals(c.nbrCands,2,"has correct total");
+});
+
+test("adding candidate",function(){
+	var c = new SS.CandList([666,0,0,0,0,0,0,0,0,0]);
+	equals(typeof c.add,"function","adding function exists");
+	equals(c.add(5),1,"adding returns number of added candidates");
+	ok(Array.compare([666,0,0,0,0,1,0,0,0,0],c.cands),"candidate list is updated with single cand");
+	equals(c.nbrCands,1,"total is updated");
+	equals(c.add([666,0,0,0,0,1,1,1,0,0]),2,"adding many returns number of added candidates");
+	ok(Array.compare([666,0,0,0,0,1,1,1,0,0],c.cands),"candidate list is updated with candlist");
+	equals(c.nbrCands,3,"total is updated");
+	equals(typeof SS.CandList.add,"function","adding function also has static counterpart");
+	equals(SS.CandList.add(c,9),1,"static add also returns number of added cands");
+	equals(c.nbrCands,4,"static add also updates nbrCands");
+});
+
+test("removing candidate",function(){
+	var c = new SS.CandList([666,1,1,1,1,1,1,1,1,1]);
+	equals(typeof c.remove,"function","removing function exists");
+	equals(c.remove(5),1,"removing returns number of removed candidates");
+	ok(Array.compare([666,1,1,1,1,0,1,1,1,1],c.cands),"candidate list is now without removed cand");
+	equals(c.nbrCands,8,"total is updated");
+	equals(c.remove([666,0,0,0,0,1,1,1,0,0]),2,"removing many returns number of removed candidates");
+	ok(Array.compare([666,1,1,1,1,0,0,0,1,1],c.cands),"candidate list is updated accordingly");
+	equals(c.nbrCands,6,"total is updated");
+	equals(typeof SS.CandList.add,"function","removing function also has static counterpart");
+	equals(SS.CandList.remove(c,1),1,"static remove also returns number of removed cands");
+	equals(c.nbrCands,5,"static remove also updates nbrCands");
+});
+
+/**************/ module("Square"); /*******************************************************/
+
+test("instantiation",function(){
+	var s = new SS.Square(5,6);
+	ok(s instanceof SS.Square," Square is constructor");
+	ok(s.candList instanceof SS.CandList,"Square has a candidatelist");
+	ok(Array.compare(s.candList.cands,[666,1,1,1,1,1,1,1,1,1]),"Candlist is full");
+	equals(s.row,"r5","Square has row property");
+	equals(s.col,"c6","Square has col property");
+	equals(s.box,"b5","Square has box property");
+	ok($.isArray(s.neighbours),"Square has neighbourids array property");
+	equals((new SS.Square(1,3).box),"b1","r1c3 has box b1");
+	equals((new SS.Square(6,4).box),"b5","r6c4 has box b5");
+	equals((new SS.Square(7,7).box),"b9","r7c7 has box b9");
+});
+
+test("classname generation",function(){
+	var s = new SS.Square();
+	equals(typeof s.getClass,"function","Square has a getClass function");
+	equals(s.getClass(),"canbe1 canbe2 canbe3 canbe4 canbe5 canbe6 canbe7 canbe8 canbe9","Starts with 'full' classname");
+	s.candList.remove(6);
+	equals(s.getClass(),"canbe1 canbe2 canbe3 canbe4 canbe5 canbe7 canbe8 canbe9","Doesn't include removed classes");
+});
+
+
+
+/**************/ module("House"); /*******************************************************/
+
+test("instantiation",function(){
+	var h = new SS.House("moo");
+	ok(h instanceof SS.House,"House is constructor");
+	equals(typeof h.candpositions, "object","house contains candpositions array");
+	equals(objpropcounter(h.candpositions),9,"candpositions object has one entry per candidate");
+	ok(h.candpositions[1] instanceof Array,"each candposition is an array");
+	equals(h.id,"moo","house was given id");
+	ok($.isArray(h.squares),"house contains squares array");
+});
+
+/**************/ module("Boardgenerator"); /*********************************************/
+
+test("board generation",function(){
+	equals(typeof SS.generateBoard, "function", "Generate function exists");
+	var board = SS.generateBoard();
+	equals(typeof board, "object", "Returns an object");
+	equals(typeof board.squares, "object", "Contains square object");
+	equals(typeof board.houses, "object", "Contains houses object");
+	equals(typeof board.moves, "object", "Contains moves object");
+	equals(typeof board.selection, "object", "Contains selection object");
+	equals(objpropcounter(board.squares),81,"squares object contains 81 squares");
+	equals(objpropcounter(board.houses),27,"houses object contains 27 houses");
+	var sqr = board.squares.r1c1, house = board.houses.r1;
+	ok(sqr instanceof SS.Square,"squares contains Square instances");
+	ok(house instanceof SS.House,"houses contains House instances");
+	ok(Array.compare(house.squares,["r1c1","r1c2","r1c3","r1c4","r1c5","r1c6","r1c7","r1c8","r1c9"]),"house has been given correct squares");
+	ok(Array.compare(house.squares,house.candpositions[1]),"candpositions are all squares");
+	ok(Array.compare(sqr.neighbours,["r1c2","r1c3","r1c4","r1c5","r1c6","r1c7","r1c8","r1c9","r2c1","r3c1","r4c1","r5c1","r6c1","r7c1","r8c1","r9c1","r2c2","r2c3","r3c2","r3c3"]),"square has been given correct neighbours");
+});
--- js/sudoku.js
+++ js/sudoku.js
@@ -5,7 +5,13 @@ if(!Array.locate){
 	 * @param {array} arr the haystack to look in
 	 * @returns {int} position of needle in array, -1 if not found
 	 */
-    Array.locate = function(obj,arr){
+    Array.locate = function(obj,arr){ // needle, haystack
+        for(var i=0, l=arr.length; i<l; i++){
+            if(arr[i]==obj){
+                return i;
+            }
+        }
+        return -1;
     };
 }
 
@@ -16,6 +22,16 @@ if (!Array.unique){
 	 * @returns {array} a new array containing only unique elements from arr
 	 */
 	Array.unique = function (arr) {
+		var r = [];
+		o:for(var i = 0, n = arr.length; i < n; i++) {
+			for(var x = 0, y = r.length; x < y; x++) {
+				if(r[x]==arr[i]) {
+					continue o;
+				}
+			}
+			r[r.length] = arr[i];
+		}
+		return r;
 	};
 }
 
@@ -27,6 +43,13 @@ if (!Array.common){
 	 * @returns {array} a new array containing elements found in both arr1 and arr2
 	 */
 	Array.common = function(arr1,arr2){
+		var r = [];
+		for (var i = 0, l = arr1.length; i < l; i++) {
+			if (Array.locate(arr1[i], arr2) != -1) {
+				r.push(arr1[i]);
+			}
+		}
+		return r;
 	};
 }
 
@@ -37,7 +60,14 @@ if (!Array.rince){
 	 * @param {array} arr2
 	 * @returns {array} a new array containing elements unique to arr1
 	 */
-	Array.rince = function(arr1,arr2){
+	Array.rince = function(arr1,arr2){ // array, filter with parts NOT to include
+		var r = [];
+		for (var i = 0, l = arr1.length; i < l; i++) {
+			if (Array.locate(arr1[i], arr2) == -1) {
+				r.push(arr1[i]);
+			}
+		}
+		return r;
 	};
 }
 
@@ -49,6 +79,15 @@ if (!Array.compare){
 	 * @returns {bool} whether or not the arrays have the same elements in the same order
 	 */
 	Array.compare = function(arr1, arr2){
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (var i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
 	};
 }
 
@@ -60,6 +99,11 @@ if (!Array.merge) {
 	 * @returns {array} a new array containing all elements from both arr1 and arr2
 	 */
 	Array.merge = function(arr1, arr2){
+		for (var i = 0; i < arr2.length; i++) {
+			if (Array.locate(arr2[i], arr1) == -1) {
+				arr1.push(arr2[i]);
+			}
+		}
 	};
 }
 
@@ -71,6 +115,10 @@ if (!Array.remove){
 	 * @param {array} array
 	 */
 	Array.remove = function(obj,array){
+		var pos = Array.locate(obj,array);
+		if (pos != -1){
+			array.splice(pos,1);
+		}
 	};
 }
 
@@ -88,34 +136,83 @@ if (!Array.remove){
 	 * @constructor
 	 * @name CandList
 	 * @class A Candidate List object
+	 * @param {void|cands} optional cands array
 	 */
-	var CandList = function(){
+	var CandList = function(cands){
 		/**
 		 * array of candidates
 		 * @name CandList#cands
 		 * @type array
 		 */
-		this.cands = "";
+		this.cands = (cands ? cands : [666,1,1,1,1,1,1,1,1,1]);
 		/**
 		 * number of remaining candidates
 		 * @name CandList#nbrCands
 		 * @type int
 		 */
-		this.nbrCands = "";
+		this.nbrCands = 0;
+		for(var i=10;--i;){
+			this.nbrCands += this.cands[i];
+		}
 	};
+	
+	// private function
+	var changeCandList = function(cand,to){
+		var changed = 0;
+		if (cand.length){
+			for(var i=10;--i;){
+				if (cand[i]) {
+					if (to != this.cands[i]) {
+						this.cands[i] = to;
+						changed++;
+					}
+				}
+			}
+		}
+		else {
+			if (to != this.cands[cand]){
+				this.cands[cand] = to;
+				changed = 1;
+			}
+		}
+		this.nbrCands += (to ? 1 : -1)*changed;
+		return changed;
+	};
+	
 	/**
 	 * sets a candidate as 1 in the list
-	 * @param {int} cand
-	 * @returns {boolean} whether or not cand wasn't already 1
+	 * @param {int|array} single or list of candidates
+	 * @returns {int} number of cands added
 	 */
 	CandList.prototype.add = function(cand){
+		return changeCandList.apply(this,[cand,1]);
 	};
+	
 	/**
 	 * sets a candidate as 0 in the list
-	 * @param {int} cand
-	 * @returns {boolean} whether or not cand wasn't already 0
+	 * @param {int|array} single or list of candidates
+	 * @returns {int} number of cands removed
 	 */
 	CandList.prototype.remove = function(cand){
+		return changeCandList.apply(this,[cand,0]);
+	};
+	/**
+	 * static version to set cand as 1 in list
+	 * @param {CandList} list the CandList to work with
+	 * @param {int|array} cand single or list of candidates
+	 * @returns {int} number of cands added
+	 */
+	CandList.add = function(list,cand){
+		return changeCandList.apply(list,[cand,1]);
+	};
+	/**
+	 * static version to set cand as 0 in list
+	 * @param {CandList} list the CandList to work with
+	 * @param {int|array} cand single or list of candidates
+	 * @returns {int} number of cands removed
+	 */
+	CandList.remove = function(list,cand){
+		return changeCandList.apply(list,[cand,0]);
 	};
 	
 	SS.CandList = CandList;
@@ -131,23 +228,29 @@ if (!Array.remove){
 	 */
 	var Square = function(r,c){
 		/**
+		 * square id (r1c1, r2c2, etc)
+		 * @name Square#id
+		 * @type string
+		 */
+		this.id = "r"+r+"c"+c;
+		/**
 		 * row id (r1, r2, etc)
 		 * @name Square#row
 		 * @type string
 		 */
-		this.row = "";
+		this.row = "r"+r;
 		/**
 		 * col id (c1, c2, etc)
 		 * @name Square#col
 		 * @type string
 		 */		
-		this.col = "";
+		this.col = "c"+c;
 		/**
 		 * box id (b1, b2, etc)
 		 * @name Square#box
 		 * @type string
 		 */
-		this.box = "";
+		this.box = "b"+(3*(Math.floor((r+2) / 3)-1) + Math.floor((c+2) / 3));
 		/**
 		 * list of all neighbour squares
 		 * @name Square#neighbours
@@ -156,10 +259,29 @@ if (!Array.remove){
 		this.neighbours = [];
 		/**
 		 * CandList of remaining candidates
-		 * @name Square#cands
+		 * @name Square#candList
 		 * @type CandList
 		 */
-		this.cands = new CandList();
+		this.candList = new CandList();
+	};
+	
+	/**
+	 * returns the CSS class string, which depends on available cands
+	 * @returns {string} the CSS class
+	 */
+	Square.prototype.getClass = function(){
+		var ret = "";
+		if (!this.answered) {
+			for (var i = 0; ++i < 10;) {
+				if (this.candList.cands[i]) {
+					ret += "canbe" + i + " ";
+				}
+			}
+		}
+		else {
+			ret = "answered is"+this.answered;
+		}
+		return ret.substr(0,ret.length-1);
 	};
 	
 	SS.Square = Square;
@@ -170,8 +292,15 @@ if (!Array.remove){
 	/**
 	 * @name House
 	 * @class a house object
+	 * @param {string} id the id of the house
 	 */
-	var House = function(){
+	var House = function(id){
+		/**
+		 * id of house
+		 * @name House#id
+		 * @type string
+		 */
+		this.id = id;
 		/**
 		 * list of all contained squares
 		 * @name House#squares
@@ -186,10 +315,20 @@ if (!Array.remove){
 		this.answeredCands = new CandList();
 		/**
 		 * possible positions for the different candidates
-		 * @name House#positions
+		 * @name House#candpositions
 		 * @type array
 		 */
-		this.positions = [];
+		this.candpositions = {
+			1: [],
+			2: [],
+			3: [],
+			4: [],
+			5: [],
+			6: [],
+			7: [],
+			8: [],
+			9: []
+		};
 	};
 	
 	SS.House = House;
@@ -208,7 +347,38 @@ if (!Array.remove){
 	 * generates board data structure
 	 */
 	SS.generateBoard = function(){
-		
+		var board = {
+			squares: {},
+			moves: {},
+			selection: {},
+			houses: {}
+		}; 
+		for (var h = 1; h < 10; h++) {
+			board.houses["r" + h] = new SS.House("r"+h);
+			board.houses["c" + h] = new SS.House("c"+h);
+			board.houses["b" + h] = new SS.House("b"+h);
+		}
+		for (var r = 1; r < 10; r++) {
+			for (var c = 1; c < 10; c++) {
+				var s = new SS.Square(r,c); 
+				board.squares[s.id] = s;
+				board.houses[s.row].squares.push(s.id);
+				board.houses[s.col].squares.push(s.id);
+				board.houses[s.box].squares.push(s.id);
+			}
+		}
+		for (h in board.houses) {
+			for (c = 1; c < 10; c++) {
+				board.houses[h].candpositions[c] = board.houses[h].squares;
+			}
+			for (s in board.houses[h].squares) {
+				Array.merge(board.squares[board.houses[h].squares[s]].neighbours, board.houses[h].squares);
+			}
+		}
+		for (s in board.squares) {
+			Array.remove(s, board.squares[s].neighbours);
+		}
+		return board;
 	};
 	
 	
